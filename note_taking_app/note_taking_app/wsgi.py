4d3eb5844bf160b91dd13254e83bc514f3ac919e
John <adeniyijohn2002@gmail.com>
2025-04-18 13:45:03 +0100

    updated file to reflect correct path of the settings file

--- note_taking_app/note_taking_app/wsgi.py
+++ note_taking_app/note_taking_app/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'note_taking_app.note_taking_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'note_taking_app.settings')
 
 application = get_wsgi_application()
