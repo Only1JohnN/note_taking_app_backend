6381edc46ee2857f57bc8de7d8fe7a7ab954c253
Adeniyi John <71540938+Only1JohnN@users.noreply.github.com>
2025-04-18 13:03:01 +0100

    Fix static files setup and update SECRET_KEY handling for production
    - Added 'whitenoise' middleware for handling static files in production.
    - Updated SECRET_KEY to be sourced from environment variables for better security.
    - Fixed typo in static files settings and ensured proper configuration for static and media files.
    - Configured static and media file paths for production deployment.

--- note_taking_app/note_taking_app/settings.py
+++ note_taking_app/note_taking_app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=qcc^q^2iqnwurtn$3!i)@tbd+ns$8fm9%#ogs$^s(f$l9u^e-'
+import os
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your_default_secret_key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',        #WhiteNoise for static files -- MUST come after django.middleware.security.SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,8 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
